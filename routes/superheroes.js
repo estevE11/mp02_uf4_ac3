a2c8c2b0063e15052ffc94bf4926db4800412885
Roger Esteve <rogeresteve30@gmail.com>
2021-05-14 16:43:45 +0200

    get /superheroes/:id

--- routes/superheroes.js
+++ routes/superheroes.js
@@ -4,23 +4,33 @@ const router = express.Router();
 const SuperheroeModel = require('./../models/SuperheroeModel');
 
 router.get('/', (req, res) => {
-    SuperheroeModel.find({}, (err, docs) => {
+    SuperheroeModel.find({}, (err, data) => {
         if (err) {
-            res.semd({ error: res });
+            res.send({ error: err });
             return;
         }
-        res.send(docs);
+        res.send(data);
+    });
+});
+
+router.get('/:id', (req, res) => {
+    SuperheroeModel.findById(req.query.id, (err, data) => {
+        if (err || !data) {
+            res.send({ error: err });
+            return;
+        }
+        res.send(data);
     });
 });
 
 router.post('/', (req, res) => {
     const superheroe = new SuperheroeModel(req.body);
-    superheroe.save((err, doc) => {
+    superheroe.save((err, data) => {
         if (err) {
             res.send({error: err});
             return;
         }
-        res.send({id: doc._id});
+        res.send({id: data._id});
     });
 });
 
